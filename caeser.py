95c052b0ac5937f45ac2948c7f899609c160ce1c
Mark Aleksander Hil <mark@100shapes.com>
2019-02-15 12:58:37 +0000

    Cipher tests pass

--- caeser.py
+++ caeser.py
@@ -1,16 +1,19 @@
 # encipher, decipher
 # use character frequency to decipher
 
-from string import ascii_lowercase
+from string import ascii_lowercase, ascii_uppercase
 
 lower = ascii_lowercase
+upper = ascii_uppercase
 
 def encipher(text, rotation):
     enciphered = []
     for i in text:
-        if i not in lower:
+        if i not in lower and i not in upper:
             enciphered.append(i)
-        else:
+        elif i in lower:
             enciphered.append(lower[(lower.index(i) + rotation) % len(lower)])
+        else:
+            enciphered.append(upper[(upper.index(i) + rotation) % len(upper)])
     enciphered = ''.join(enciphered)
     return enciphered
