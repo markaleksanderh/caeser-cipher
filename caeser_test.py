00880af1421281f6a6b9984121eb22d1eb20d902
Mark Aleksander Hil <mark@100shapes.com>
2019-02-15 12:50:19 +0000

    re-order asserts in test

--- caeser_test.py
+++ caeser_test.py
@@ -3,9 +3,14 @@ from caeser import *
 def test_encipher_lowercase():
     text = 'hello world'
     ciphered = 'uryyb jbeyq'
-    assert encipher(text, 13) == ciphered
+    assert ciphered == encipher(text, 13)
 
-# def test_encipher_uppercase():
-#     text = 'HELLO WORLD'
-#     ciphered = 'IFMMP XPSME'
-#     assert encipher(text, 1) == ciphered
+def test_encipher_uppercase():
+    text = 'HELLO WORLD'
+    ciphered = 'IFMMP XPSME'
+    assert ciphered == encipher(text, 1)
+
+def test_encipher_special_characters():
+    text = 'H2142341ello! wor?...,,LD++'
+    ciphered = 'O2142341lssv! dvy?...,,SK++'
+    assert ciphered == encipher(text, 7)
