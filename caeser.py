c5557329ac038a342972d9ab3ffeb06d45f8744d
Mark Aleksander Hil <mark@100shapes.com>
2019-02-15 12:43:40 +0000

    lowercase encipher test passed

--- caeser.py
+++ caeser.py
@@ -1,7 +1,16 @@
 # encipher, decipher
 # use character frequency to decipher
 
+from string import ascii_lowercase
+
+lower = ascii_lowercase
 
 def encipher(text, rotation):
-    enciphered = ''
+    enciphered = []
+    for i in text:
+        if i not in lower:
+            enciphered.append(i)
+        else:
+            enciphered.append(lower[(lower.index(i) + rotation) % len(lower)])
+    enciphered = ''.join(enciphered)
     return enciphered
--- caeser_test.py
+++ caeser_test.py
@@ -1,6 +1,11 @@
 from caeser import *
 
-def test_encipher():
-    text = 'Hello World'
-    ciphered = 'Uryyb Jbeyq'
+def test_encipher_lowercase():
+    text = 'hello world'
+    ciphered = 'uryyb jbeyq'
     assert encipher(text, 13) == ciphered
+
+# def test_encipher_uppercase():
+#     text = 'HELLO WORLD'
+#     ciphered = 'IFMMP XPSME'
+#     assert encipher(text, 1) == ciphered
